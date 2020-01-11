23d9979f37c7a2eefc0b89c28fc20511b71e1239
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-11 01:59:32 -0500

    añadiendo menu

--- app/src/main/java/com/example/u1ejerciciopropuesto2/MainActivity.java
+++ app/src/main/java/com/example/u1ejerciciopropuesto2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.u1ejerciciopropuesto2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true; /** true -> el menú ya está visible */
+    }
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.accion_salir) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
